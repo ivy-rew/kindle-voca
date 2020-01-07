ba9f826d2994ca6edb553696ed992b470ecb4ecc
LordEagle <r.wermelinger@gmail.com>
2020-01-07 16:13:19 +0100

    separate unknown and learned words
    do it as kindle does it : set category 100

--- learn.sh
+++ learn.sh
@@ -8,14 +8,25 @@ function quoteBook()
   BOOK_QUERY="SELECT l.usage, b.title, b.authors FROM LOOKUPS l
 JOIN BOOK_INFO b ON b.id = l.book_key
 WHERE l.word_key == 'en:$WORD'"
-  sqlite3 $DB "$BOOK_QUERY"
+  QUOTE=$(sqlite3 $DB "$BOOK_QUERY")
+  grep --color $WORD <<< $QUOTE
+}
+
+function archive()
+{
+  WORD=$1
+  ARCHIVE_SQL="UPDATE WORDS 
+      SET category=100 
+      WHERE word='$WORD';"
+  sqlite3 $DB "$ARCHIVE_SQL"
 }
 
 function ask()
 {
   WORD=$1
   echo $WORD
-  select OPERATION in 'learned' 'quote (kindle)' 'lookup (leo)'
+  quoteBook $WORD
+  select OPERATION in 'next' 'archive' 'quote (kindle)' 'lookup (leo)'
     do
         if [ "$OPERATION" == "lookup (leo)" ]; then
             echo "asking leo for meaning of: $WORD"
@@ -24,20 +35,67 @@ function ask()
         if [ "$OPERATION" == "quote (kindle)" ]; then
             quoteBook $WORD
         fi
-        if [ "$OPERATION" == "learned" ]; then
+        if [ "$OPERATION" == "archive" ]; then
+            archive $WORD
+            break;
+        fi
+        if [ "$OPERATION" == "next" ]; then
             break;
         fi
   done
 }
 
+function selectWords()
+{
+  CATEGORY=$1
+  WORDLIST_QUERY="SELECT word 
+    FROM words
+    WHERE category$CATEGORY
+    ORDER BY timestamp DESC"
+  WORDS=$(sqlite3 -newline ' ' $DB "$WORDLIST_QUERY")
+  echo $WORDS
+}
+
+function askMode()
+{
+  RESULT="=0"
+  select MODE in 'archived' 'open' 'all'
+  do
+    if [ "$MODE" == "archived" ]; then
+        RESULT='=100'
+    fi
+    if [ "$MODE" == "open" ]; then
+        RESULT='=0'
+    fi
+    if [ "$MODE" == "all" ]; then
+        RESULT='>=0'
+    fi
+    break;
+  done
+  echo $RESULT
+}
+
+function installDeps()
+{
+  if ! [ -x "$(command -v leo)" ]; then
+    sudo apt install -y libwww-dict-leo-org-perl
+  fi
+  if ! [ -x "$(command -v sqlite3)" ]; then
+    sudo apt install -y sqlite3
+  fi
+}
+
 function main()
 {
-    WORDLIST_QUERY="SELECT substr(l.word_key, 4) as word FROM LOOKUPS l"
-    WORDS=$(sqlite3 -newline ' ' $DB "$WORDLIST_QUERY")
-    for WORD in $WORDS
-    do
-      ask $WORD
-    done
+  installDeps
+
+  echo "Which words do you want to train?"
+  MODE=$(askMode)
+  WORDS=$(selectWords $MODE)
+
+  for WORD in $WORDS; do
+    ask $WORD
+  done
 }
 
 main
