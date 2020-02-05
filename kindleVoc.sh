8285c07283e486cfabf9fcf5af8fa43e3b191fb8
LordEagle <r.wermelinger@gmail.com>
2020-02-05 08:28:19 +0100

    fix: same stem different word (e.g. pebble)

--- kindleVoc.sh
+++ kindleVoc.sh
@@ -6,16 +6,17 @@ function quoteBook()
 {
   STEM=$1
   BOOK_QUERY="SELECT replace(l.usage,\"
-\",\" \"), b.title, b.authors, w.word FROM LOOKUPS l
-JOIN BOOK_INFO b ON b.id = l.book_key
-JOIN WORDS w on w.id = l.word_key
-WHERE w.stem == '${STEM}'"
+\",\" \"), b.title, b.authors, w.word 
+    FROM WORDS w
+    JOIN BOOK_INFO b ON b.id = l.book_key
+    JOIN LOOKUPS l on w.id = l.word_key
+    WHERE w.stem == '${STEM}'"
   RESR=$(sqlite3 -separator ' | ' $DB "$BOOK_QUERY")
   readarray -t RES <<< ${RESR}
-  WORD=$(echo "${RES[0]}" | awk -F "| " '{print $NF}')
-  WLEN=${#WORD}
-  CUT=$(($WLEN+2))
   for Q in "${RES[@]}"; do
+    WORD=$(echo "${Q}" | awk -F "| " '{print $NF}')
+    WLEN=${#WORD}
+    CUT=$(($WLEN+2))
     QUOTE="${Q:0:-${CUT}}"
     echo "${QUOTE}" | grep --color "$WORD"
   done
@@ -33,7 +34,7 @@ function archive()
 function selectWords()
 {
   CATEGORY=$1
-  WORDLIST_QUERY="SELECT stem 
+  WORDLIST_QUERY="SELECT DISTINCT stem 
     FROM words
     WHERE category$CATEGORY
     ORDER BY timestamp DESC"
